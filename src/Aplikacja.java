6bddcafa269663289ad44943abb2dfa399bcd06c
TymonScibiorski <tym9128@gmail.com>
2025-03-17 22:02:09 +0100

    Wrote titles to application's required functions

--- src/Aplikacja.java
+++ src/Aplikacja.java
@@ -3,4 +3,39 @@ import java.util.ArrayList;
 public class Aplikacja{
     public ArrayList<Kontener> kontenery = new ArrayList<>();
 
+    public void createKontener(){
+
+    }
+
+    public void loadKontenerWith(int masa_ladunku_kg, ladunek typ_ladunku){
+
+    }
+
+    public void loadKontenerOnShip(Kontener kontener, Kontenerowiec kontenerowiec){
+
+    }
+
+    public void loadListOfKontenersOnShip(ArrayList<Kontener> kontener, Kontenerowiec kontenerowiec){
+
+    }
+
+    public void removeKontenerFromShip(Kontener kontener, Kontenerowiec kontenerowiec){
+
+    }
+
+    public void unloadKontenerowiec(Kontenerowiec kontenerowiec){
+
+    }
+
+    public void swapKonteners(Kontener kontener1, Kontener kontener2){
+
+    }
+
+    public String getInfoKontener(Kontener kontener){
+        return null;
+    }
+
+    public String getInfoKontenerowiecAndLoad(Kontenerowiec kontenerowiec){
+        return null;
+    }
 }
--- src/Kontenerowiec.java
+++ src/Kontenerowiec.java
@@ -6,5 +6,21 @@ public class Kontenerowiec {
     int max_amountof_kontenery;
     int max_kontenery_mass_tn;
 
+    public ArrayList<Kontener> getKontenery_na_pokladzie() {
+        return kontenery_na_pokladzie;
+    }
+
+    public int getMax_speed_kts() {
+        return max_speed_kts;
+    }
+
+    public int getMax_amountof_kontenery() {
+        return max_amountof_kontenery;
+    }
+
+    public int getMax_kontenery_mass_tn() {
+        return max_kontenery_mass_tn;
+    }
+
 
 }
